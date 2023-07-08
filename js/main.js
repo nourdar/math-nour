c86dfd2eb77167647c299b49d737fd84c3ffea2d
GACHTOU NourEddine <gachtoun@gmail.com>
2023-07-08 18:04:22 +0100

    one step forward

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ let calculate = document.getElementById("calculate")
 
 let fInputs = document.getElementById("f-inputs") // div container of functions inputs
 
-
+let vec = {
+    x: 1,
+    y: 2,
+}
 
 m.addEventListener('change', function (e) {
     // fInputs.innerHTML = ""
 
     let f = ""
+
+
 
     for (i = 0; i < m.value; i++) {
 
@@ -20,8 +25,11 @@ m.addEventListener('change', function (e) {
 
 
         f += '<br>'
-
+        f += '<div style="display:none" class="resultat-div " > Resultat pour <strong>x = ' + vec.x + ' </strong> -  <strong>y = ' + vec.y + ' </strong>: <span id="res-' + i + '"><span>  </div>'
+        f += '<div style="display:none" class="resultat-div " > Derivative est : <span id="res-der-' + i + '"><span>  </div>'
         f += '<br>'
+        f += '<br>'
+
 
     }
 
@@ -33,15 +41,34 @@ m.addEventListener('change', function (e) {
 
 
 calculate.addEventListener('click', function (e) {
-    let fImage = document.getElementById("f-image-1")
 
-    console.log(p(math.derivative(fImage.value, 'x')))
+    for (i = 0; i < m.value; i++) {
+
+        let fvar = document.getElementById("f-var-" + i)
+        let fImage = document.getElementById("f-image-" + i)
+        let res = document.getElementById("res-" + i)
+        let resDer = document.getElementById("res-der-" + i)
+
+        fvar.value = 'x,y' // just for test purpose, it should be dynamic
 
 
+        res.innerHTML = math.evaluate(fImage.value, vec)
+        resDer.innerHTML = '( ' + p(math.derivative(fImage.value, 'x')) + ' ,' + p(math.derivative(fImage.value, 'y')) + ')'
+
+        res.parentElement.style.display = 'block'
+        resDer.parentElement.style.display = 'block'
+        // console.log(func, math.evaluate(func, vec))
+        // console.log(p(math.derivative(fImage.value, 'x')))
+
+    }
 })
 
 
 function p(value) {
     var precision = 14;
     return math.format(value, precision);
+}
+
+function vector(vector, size) {
+
 }
